e1ac2d9e3592fc22dc8c9ef16b379bc152b0ddd7
Patel purv <patelpurv2643@gmail.com>
2025-03-10 15:40:24 +0530

    nav added

--- nikeclone/src/App.jsx
+++ nikeclone/src/App.jsx
@@ -1,16 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  CustomerReviews, Hero, PopularProducts,
+  Services, Subscribe, SuperQuality, Footer, SpecialOffer
+} from './section'  // Importing all sections from the sections folder using index.js
 
-function App() {
-  
+import Nav from './Components/Nav'
 
-  return (
-    <>
-    <p class="bg-slate-200">hello world</p>
-    </>
-  )
-}
+const App = () => (
+  <main className="relative">  {/* Added relative class to the main tag  so, nav can be absolute to thie main instead of webpage */}
+
+    <Nav />
+
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+
+    <section className="padding">
+      <PopularProducts />
+    </section>
+
+    <section className="padding">
+      <SuperQuality />
+    </section>
+
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+
+    <section className="bg-pale-blue padding">
+      < CustomerReviews />
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+
+  </main>
+)
 
 export default App
