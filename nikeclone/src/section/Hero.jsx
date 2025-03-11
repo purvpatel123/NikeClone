3aa9b180fd5510366cdac62644859708ef654ed0
Patel purv <patelpurv2643@gmail.com>
2025-03-11 16:05:14 +0530

    popularproduct added

--- nikeclone/src/section/Hero.jsx
+++ nikeclone/src/section/Hero.jsx
@@ -1,8 +1,11 @@
 import Button from "../Components/Button"
 import { arrowRight } from "../assets/icons"
-import { statistics } from "../constants"
-import {bigShoe1} from "../assets/images"
+import { statistics,shoes } from "../constants"
+import { bigShoe1 } from "../assets/images"
+import ShoeCard from "../Components/ShoeCard"
+import { useState } from "react"
 const Hero = () => {
+  const [bigShoeImage, setBigShoeImage] = useState(bigShoe1);
   return (
     <section
       id="home"
@@ -18,7 +21,7 @@ const Hero = () => {
     max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
           our summer Collection</p>
-          <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
           <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
             The New Arrival
           </span>
@@ -40,16 +43,30 @@ const Hero = () => {
           ))}
         </div>
       </div>
-<div className="relative flex-1 flex justify-center items-center
+      <div className="relative flex-1 flex justify-center items-center
  xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-  <img
-  src={bigShoe1}
-  alt="shoe collection"
-  width={610}
-  height={500}
-  className="object-contain relative z-10"
- />
-</div>
+        <img
+          src={bigShoeImage}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div className="flex sm:gap-6 gap-4 absolute
+        -bottom-[5%]
+        sm:left-[19%] max-sm:px-6
+        ">
+          {shoes.map((shoe, index) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImage(shoe)}
+                bigShoeImage={bigShoeImage}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   )
 }
